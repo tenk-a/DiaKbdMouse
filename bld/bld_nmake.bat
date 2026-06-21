2be510cb9c3cacd865e2d3217a5ba8ecb217e44c
tenk-a <tenka@6809.net>
2026-06-22 02:02:43 +0900

    旧win対応のためデフォルトの vc を v14.1(2017) に変更. nmakeでのビルドオプション調整

--- bld/bld_nmake.bat
+++ bld/bld_nmake.bat
@@ -3,7 +3,9 @@ cd ..
 where cl >nul 2>nul || (echo Run this from a Visual C++ Developer Command Prompt. & popd & exit /b 1)
 where nmake >nul 2>nul || (echo nmake was not found in PATH. & popd & exit /b 1)
 set tgt=nmake
-if /I "%1"=="xp" set tgt=nmake-xp
+if /I "%1"=="xp"    set tgt=nmake-xp
+if /I "%1"=="win64" set tgt=nmake-win64
 cmake --preset %tgt%
 cmake --build --preset %tgt%-install
+if /I "%tgt%"=="nmake-win64" cmake --build --preset %tgt%-package
 popd
